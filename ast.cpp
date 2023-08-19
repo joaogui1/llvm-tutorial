3f54da556d3243df20f4cd0b6ae9edf3cc60be47
joaogui1 <joaoguilhermearujo@gmail.com>
2023-08-19 14:07:06 -0400

    testing c++ understanding

---
+++ ast.cpp
@@ -0,0 +1,84 @@
+#include <string>
+#include <iostream>
+
+class ExprAST {
+public:
+    virtual ~ExprAST() = default;
+};
+
+/// NumberExprAST - Expression class for numeric literals like "1.0".
+class NumberExprAST : public ExprAST {
+    double Val;
+
+public:
+    NumberExprAST(double Val) : Val(Val) {}
+    auto Repr(){
+        return Val;
+    }
+};
+
+class VariableExprAST : public ExprAST {
+    std::string Name;
+
+public:
+    VariableExprAST(const std::string &Name) : Name(Name) {}
+    auto Repr(){
+        return Name;
+    }
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+    char Op;
+    std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+    BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                    std::unique_ptr<ExprAST> RHS)
+        : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+    CallExprAST(const std::string &Callee,
+                std::vector<std::unique_ptr<ExprAST>> Args)
+    : Callee(Callee), Args(std::move(Args)) {}
+};
+
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the function takes).
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+        : Name(Name), Args(std::move(Args)) {}
+
+    const std::string &getName() const { return Name; }
+};
+
+/// FunctionAST - This class represents a function definition itself.
+class FunctionAST {
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body) : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
+int main(){
+    auto n = NumberExprAST(0.4);
+    auto LHS = VariableExprAST("x");
+    // auto LHS = std::make_unique<VariableExprAST>("x");
+    // auto RHS = std::make_unique<VariableExprAST>("y");
+    // auto Result = std::make_unique<BinaryExprAST>('+', std::move(LHS),
+    //                                           std::move(RHS));
+    std::cout << n.Repr() << " " << LHS.Repr() << "\n";
+    return 0;
+}
